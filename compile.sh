c1b0687ff936843e58002892970e3c89d9f035f0
icexin <fanbingxin.me@gmail.com>
2021-07-14 00:07:18 +0800

    README and LICENSE

--- compile.sh
+++ compile.sh
@@ -14,14 +14,6 @@ function compile {
 	sed -i -e 's/var win_bounds/var win_bounds Nk_rect/' $target
 	sed -i -e 's/var iter_bounds/var iter_bounds Nk_rect/' $target
 	sed -i -e 's|modernc.org/libc|github.com/icexin/nk/libc|' $target
-	# sed -i -e 's/tls \*TLS,//g' $target
-	# sed -i -e 's/tls \*TLS//g' $target
-	# sed -i -e 's/\*TLS,//g' $target
-	# sed -i -e 's/\*TLS//g' $target
-	# sed -i -e 's/tls\.Alloc/tlsAlloc/g' $target
-	# sed -i -e 's/tls\.Free/tlsFree/g' $target
-	# sed -i -e 's/tls,//g' $target
-	# sed -i -e 's/(tls)/()/g' $target
 
 	go fmt $target
     mv cnk/capi_${TARGET_GOOS}_${TARGET_GOARCH}.go cnk/capi_${TARGET_GOARCH}.go
